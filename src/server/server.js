aee48a9d9d1295fa0fd6ecd9505c68c6dca4adb2
naraku <570453516@qq.com>
2016-04-18 09:07:38 +0800

    add mongoose connect

--- src/server/server.js
+++ src/server/server.js
@@ -1,22 +1,23 @@
 'use strict';
-var fs = require('fs');
-var path = require('path');
-var koa = require('koa');
-var logger = require('koa-logger');
-var serve = require('koa-static');
-var convert = require('koa-convert');
-var send = require('koa-send');
-var router = require('koa-router')();
-var env = process.argv[2];
-var debug = 'dev' == env;
-var app = new koa();
-var staticPath = path.resolve(__dirname, '../' + (debug ? '../' : '../dist'));
+let fs = require('fs');
+let path = require('path');
+let koa = require('koa');
+let mongoose = require('mongoose');
+let logger = require('koa-logger');
+let serve = require('koa-static');
+let convert = require('koa-convert');
+let send = require('koa-send');
+let router = require('koa-router')();
+let env = process.argv[2];
+let debug = 'dev' == env;
+let app = new koa();
+let staticPath = path.resolve(__dirname, '../' + (debug ? '../' : '../dist'));
 app.use(serve(staticPath));
 if (debug) {
-    var webpackDevMiddleware = require('koa-webpack-dev-middleware')
-    var webpack = require('webpack')
-    var webpackConf = require('../../build_config/webpack.dev.config.js');
-    var compiler = webpack(webpackConf)
+    let webpackDevMiddleware = require('koa-webpack-dev-middleware')
+    let webpack = require('webpack')
+    let webpackConf = require('../../build_config/webpack.dev.config.js');
+    let compiler = webpack(webpackConf)
 
     // 为使用Koa做服务器配置koa-webpack-dev-middleware
     app.use(webpackDevMiddleware(compiler, {
@@ -28,13 +29,17 @@ if (debug) {
     }))
 
     // 为实现HMR配置webpack-hot-middleware
-    var hotMiddleware = require("webpack-hot-middleware")(compiler);
+    let hotMiddleware = require("webpack-hot-middleware")(compiler);
     // Koa对webpack-hot-middleware做适配
     app.use(function*(next) {
         yield hotMiddleware.bind(null, this.req, this.res);
         yield next;
     });
 }
+mongoose.connect('mongodb://localhost/mkrn');
+mongoose.connection.on("error", function(err) {
+  console.log(err);
+});
 // Logger
 app.use(logger());
 // app.use(router.routes())
